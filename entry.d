0cc612dc056f761a1264b9207de6359bfabdad2d
jimtahu <jimtahu@gmail.com>
2012-05-27 21:48:56 -0400

    Added check for program file in args.

--- entry.d
+++ entry.d
@@ -4,6 +4,10 @@ import std.file;
 
 int main(string[] args){
     char opp,src,dst;
+    if(args.length<2){
+    	writefln("Usage: %s <programfile>",args[0]);
+	return 1;
+    }//end if not enough items
     auto i7 = new cpu(cast(byte [])read(args[1]));
     return i7.run();
 }//end main
