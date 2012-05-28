9fbdd5134ad416c4a8a84de70a68b2d045795081
jimtahu <jimtahu@gmail.com>
2012-05-28 11:39:13 -0400

    Added magic bytes to program header.

--- entry.d
+++ entry.d
@@ -2,13 +2,16 @@ import cpu;
 import std.stdio;
 import std.file;
 
+const int MAGIC_HEAD=3;
+
 int main(string[] args){
-    char opp,src,dst;
+    byte[] prog;
     if(args.length<2){
     	writefln("Usage: %s <programfile>",args[0]);
 	return 1;
     }//end if not enough items
-    auto i7 = new cpu(cast(byte [])read(args[1]));
+    prog = cast(byte [])read(args[1]);
+    auto i7 = new cpu(prog[MAGIC_HEAD..$]);
     return i7.run();
 }//end main
 
